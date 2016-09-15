87a7ac967321940246eccb40180f3bad522eed8b
Mukesh Rawat <mukeshrawat02@gmail.com>
2016-09-15 12:05:52 +0100

    Added API Controller and respective model classes. Also marked it EnableCors for Enabling Cross-Origin Requests.

---
+++ WeatherApp.Server/ApiControllers/WeatherController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WeatherApp.Server.Models;
+using WeatherApp.Server.Services;
+
+namespace WeatherApp.Server.ApiControllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class WeatherController : ApiController
+    {
+        [Route("api/weather/{location}")]
+        public IEnumerable<WeatherDetail> Get(string location)
+        {
+            return OpenWeatherMapService.GetWeatherForecast(location);
+        }
+    }
+}
---
+++ WeatherApp.Server/Models/WeatherDetail.cs
@@ -0,0 +1,16 @@
+﻿namespace WeatherApp.Server.Models
+{
+    public class WeatherDetail
+    {
+        public string weather { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+        public string date { get; set; }
+        public string dayOfWeek { get; set; }
+        public string temp { get; set; }
+        public string maxTemp { get; set; }
+        public string minTemp { get; set; }
+        public string windSpeed { get; set; }
+        public string humidity { get; set; }
+    }
+}
